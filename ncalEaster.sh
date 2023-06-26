2e6a94d094761ef9f827acb7b429f73f971b51a1
Gregory Mirsky <6909025+gmirsky@users.noreply.github.com>
2023-06-26 12:58:03 -0400

    formatting

--- ncalEaster.sh
+++ ncalEaster.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Step#1 - Parse the input
+# Step #1 - Parse the input
 eval "$(jq -r '@sh "yyyy=\(.yyyy)"')"
 
-# Step#2 - Generate the output
+# Step #2 - Generate the output
 jq -n --arg year "$yyyy" \
       --arg month "$(ncal -e $yyyy | awk '{print $(NF-2)}' | xargs)" \
       --arg day   "$(ncal -e $yyyy | awk '{print $(NF-1)}' | xargs)" \
